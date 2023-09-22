d3968ecee7e0a1883580c07a7e8c7844670e45aa
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-09-21 18:35:19 -0600

    setup fixes

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 const http = require("http");
+const dotenv = require("dotenv").config();
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -6,7 +7,8 @@ const port = 3000;
 const server = http.createServer((req: any, res: any) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end(`key: ${process.env.OPENAI_API_KEY}`);
+  // res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+    res.end(`key: ${process.env.OPENAI_API_KEY}`);
+    // res.end("Hello World");
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
